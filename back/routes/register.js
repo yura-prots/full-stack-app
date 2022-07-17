340e5b75e9677cf5f2cdff07ad190a9fc9917a0b
yura-prots <yuraprots@gmail.com>
2022-07-17 09:49:18 +0300

    add mongodb

--- back/routes/register.js
+++ back/routes/register.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
-   res.send({ message: `Hello ${req.body.email}! You are registered.` });
+   res.send(`Hello ${req.body.email}! You are registered.`);
 });
 
 module.exports = router;
